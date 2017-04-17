55b5cecdad78b66718d9a07c4eef8094819c3823
Salvatore McCarty <salvatore.mccarty@gmail.com>
2017-04-17 03:01:45 +0000

    updated history maybe?

--- pages/lobby/lobby.ts
+++ pages/lobby/lobby.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { QuestionPage } from '../question-page/question-page';
+import { HistoryPage } from '../history/history';
 /**
  * Generated class for the Lobby page.
  *
@@ -24,5 +25,8 @@ export class Lobby {
   takeTest() {
     this.navCtrl.push(QuestionPage);
   }
-
+  
+  myHistory() {
+    this.navCtrl.push(HistoryPage);
+  }
 }
