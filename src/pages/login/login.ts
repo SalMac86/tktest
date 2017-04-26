766e0a1050116c66207ae0be710967b5cf29b19e
jbrownssf <jpbrown@softstackfactory.org>
2017-04-26 00:06:38 +0000

    still not working, but want to save branch

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Lobby } from '../lobby/lobby';
+
+import { AppUsers } from '../../providers/app-users';
 /**
  * Generated class for the Login page.
  *
@@ -13,12 +16,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class Login {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  user: any = {}
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public appUsers: AppUsers
+    ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Login');
+  }
+
+  loginForm(form){
+    console.log(form);
+    if(form.invalid){
+      return alert("Please fill in all required fields!");
+    }
+    this.appUsers.login(this.user)
+      .map(res => res.json())
+      .subscribe(res => {
+        window.localStorage.setItem('token', res.id);
+        window.localStorage.setItem('userId', res.userid);
+        this.navCtrl.setRoot(Lobby);
+        //handle successful responses
+        // console.log("hope this worked");
+      }, 
+      error => {
+      // switch(error.status) {
+        // case 404:
+        //   alert("404: Page Not Found");
+        //   break;
+        // case 422:
+        //   alert("That e-mail is already in use.");
+        //   break;
+        // case null:
+        //   alert("User is offline.");
+        //   break;
+        // case 500:
+        //   alert("THE SKY IS FALLING!!!");
+        //   break;
+        // default:
+          console.log("nope, didn't work cuz: "+error.status);
+          // break;
+      })
+
+    //do stuff
+    
   }
 
 }
--- src/providers/app-users.ts
+++ src/providers/app-users.ts
@@ -16,13 +16,20 @@ export class AppUsers {
     ) {
     console.log('Hello AppUser Provider');
   }
-  baseUrl: string = "http://sp-17-salvatore-jbrownssf.c9users.io/api"
+  baseUrl: string = "http://sp-17-salvatore-jbrownssf.c9users.io:8080/api"
   path: string = "/AppUsers"
   
   register(newUserData) {
     return this.http.post(
       this.baseUrl + this.path,
       newUserData
+      )
+  }
+  
+  login(userData) {
+    return this.http.post(
+      this.baseUrl + this.path + "/login",
+      userData
       )
   }
 }
